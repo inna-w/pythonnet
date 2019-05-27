30915400de173632a8b69daac5a562049c6dac4d
Inna Wiesel <inna-w@novameasuring.com>
2019-05-27 17:56:31 +0300

    Don't configure output directory for now

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -2,4 +2,4 @@ $project_file = ".\src\runtime\Python.Runtime.csproj"
 
 Write-Host "Creating NuGet package from $project_file"
 
-& .\tools\nuget\nuget.exe pack $project_file -OutputDirectory=.\nuget
+& .\tools\nuget\nuget.exe pack $project_file
