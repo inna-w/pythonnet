1816ab3b95bb583702fce3d6ca584c74b74432fc
Inna Wiesel <inna-w@novameasuring.com>
2019-05-28 16:11:19 +0300

    Output xplat nuget package to nuget directory

--- setup.py
+++ setup.py
@@ -351,6 +351,7 @@ class BuildExtPythonnet(build_ext.build_ext):
             _xbuild,
             os.path.join("src", "runtime", "Python.Runtime.15.csproj"),
             "/p:PackageId=pythonnet_py{0}{1}_{2}".format(PY_MAJOR, PY_MINOR, ARCH),
+            "/p:PackageOutputPath={}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nuget")),
             "/p:Configuration={}".format(_config),
             "/p:Platform={}".format(ARCH),
             '/p:{}DefineConstants="{}"'.format(
